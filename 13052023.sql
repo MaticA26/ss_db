02b7094167c5f4b20f54ade36afb4bf41a63beaf
unknown <andrei.matica00@e-uvt.ro>
2023-05-13 15:46:44 +0300

    13.05.2023

---
+++ 13052023.sql
@@ -0,0 +1,14 @@
+---create table regiuni_2 as
+---select region, Count(*) nr_judete,ST_Union(geom) geom from judete group by region;
+
+
+---alter table judete add column area DECIMAL;
+
+---update judete set area=ST_Area(geom)/10000;
+
+---select * from judete;
+
+
+---select MIN(pop2011), MAX(pop2011), SUM(pop2011) from judete where region='Centru';
+
+select region, SUM(pop2011) populatie_2011, ST_Union(geom) geom from judete group by region;
